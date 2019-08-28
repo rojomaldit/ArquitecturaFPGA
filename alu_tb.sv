5771f2881d6c98e2f763f740832ea88ae2e88684
MATEO FRANCISCO OSTORERO <mostorero294@lab08.famaf.unc.edu.ar>
2019-08-28 10:22:21 -0300

    added alu tb

--- alu_tb.sv
+++ alu_tb.sv
@@ -1,22 +1,40 @@
 module alu_tb();
-	logic [63:0] a,b;
-	logic [3:0]ALUControl;
-	logic [63: 0]result, zero;
-
-	ALU test(a, b, ALUControl, result, zero);
-
+	logic [63:0] a,b,result,zero;
+	logic [3:0] ALUC;
+	alu test(a,b,ALUC,result,zero);
 	initial begin
-		a = 64'b0 + 3;
-		b = 64'b0 + 3;
-		ALUControl = 4'b0000; #10;
-		ALUControl = 4'b0001; #10;
-		ALUControl = 4'b0010; #10;
-		ALUControl = 4'b0110; #10;
-		ALUControl = 4'b0111; #10;
-		ALUControl = 4'b1100; #10;
-		ALUControl = 4'b1111; #10;
+		a = 3; b = 3;
+		ALUC = 4'b0000; #10;
+		ALUC = 4'b0001; #10;
+		ALUC = 4'b0010; #10;
+		ALUC = 4'b0110; #10;
+		ALUC = 4'b0111; #10;
+		ALUC = 4'b1100; #10;
+		ALUC = 4'b1111; #10;
+		a = -3; b = 3;
+		ALUC = 4'b0000; #10;
+		ALUC = 4'b0001; #10;
+		ALUC = 4'b0010; #10;
+		ALUC = 4'b0110; #10;
+		ALUC = 4'b0111; #10;
+		ALUC = 4'b1100; #10;
+		ALUC = 4'b1111; #10;
+		a = 3; b = -3;
+		ALUC = 4'b0000; #10;
+		ALUC = 4'b0001; #10;
+		ALUC = 4'b0010; #10;
+		ALUC = 4'b0110; #10;
+		ALUC = 4'b0111; #10;
+		ALUC = 4'b1100; #10;
+		ALUC = 4'b1111; #10;
+		a = -3; b = -3;
+		ALUC = 4'b0000; #10;
+		ALUC = 4'b0001; #10;
+		ALUC = 4'b0010; #10;
+		ALUC = 4'b0110; #10;
+		ALUC = 4'b0111; #10;
+		ALUC = 4'b1100; #10;
+		ALUC = 4'b1111; #10;
 	end
 endmodule
-	
-	
-
+		
