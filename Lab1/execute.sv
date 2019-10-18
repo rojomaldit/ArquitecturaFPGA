7c3b4f231b97f205a340a2504a15675f324e2646
rojomaldit <lucaserfurt@hotmail.com>
2019-10-18 10:33:00 -0300

    Started project

--- Lab1/execute.sv
+++ Lab1/execute.sv
@@ -1,10 +1,10 @@
-	module execute ( input logic AluSrc,
+	module execute #(parameter size = 64)( input logic AluSrc,
                     input logic[3:0] AluControl,
-                    input logic[63:0] PC_E, signImm_E, readData1_E, readData2_E,
-                    output logic[63:0] PCBranch_E, aluResult_E, writeData_E, 
+                    input logic[size-1:0] PC_E, signImm_E, readData1_E, readData2_E,
+                    output logic[size-1:0] PCBranch_E, aluResult_E, writeData_E, 
                     output logic zero_E);
 	
-    logic[63:0] muxOut;
+    logic[size-1:0] muxOut;
 
     assign writeData_E = readData2_E;
     mux2 _mux(readData2_E, signImm_E, AluSrc, muxOut);
