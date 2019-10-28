4e1aac731bd312fada9dff8c9bd116b8b7296b52
mat_ost@hotmail.com <mat_ost@hotmail.com>
2019-10-28 15:16:50 -0300

    Make some advances in lab3

--- Lab2/armv8_createAssembly/main.s
+++ Lab2/armv8_createAssembly/main.s
@@ -1,4 +1,25 @@
 	.text
 	.org 0x0000
-label:  add x1,x0,x1
-	b label
+        stur x0,[x0] 
+        stur x1,[x0,#8] 
+        stur x2,[x0,#16] 
+        stur x3,[x0,#24]
+        stur x4,[x0,#32]
+        stur x5,[x0,#40]
+        stur x6,[x0,#48]
+        ldur x7,[x0] 
+        ldur x8,[x0,#8] 
+        ldur x9,[x0,#16] 
+        ldur x10,[x0,#24]
+        ldur x11,[x0,#32]
+        ldur x12,[x0,#40]
+        ldur x13,[x0,#48]
+        sub x14,x14,x14 
+        cbz x14, loop
+        nop
+        nop
+        nop
+        nop
+        sub x15,x0,x1 
+loop:   add x15,x0,x1
+        stur x15, [x0,#56]
