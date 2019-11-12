d7e6556034bfdff0b5cdae13a82187bc13c02609
octaviotastico <octaviopercivaldi2@gmail.com>
2019-11-12 10:19:21 -0300

    Solved some errors

--- Lab3/fetch.sv
+++ Lab3/fetch.sv
@@ -5,9 +5,9 @@ module fetch #(parameter size = 64)(input logic PCSrc_F,clk,reset,
 
   logic[size-1:0] muxOut,flopOut,adderOut;
 
-  // flopr  #(size) flop(clk,reset,muxOut,flopOut);
   mux2 #(size) _mux(adderOut,PCBranch_F,PCSrc_F,muxOut);
-  flopre #(size) _flopre(clk,reset,enable,muxOut,imem_addr_F);
+  flopre #(size) _flopre(clk,reset,enable,muxOut,adderOut);
+
   assign adderOut = flopOut + 4;
   assign imem_addr_F = flopOut;
 
